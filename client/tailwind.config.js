bbdb946017f99d7c24b47b3757e650a38dcc2c1a
arpitp23 <arpitpal2306@gmail.com>
2026-01-23 00:44:33 +0530

    Add files via upload

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#4cbbb9',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
